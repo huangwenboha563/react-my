09f42e4af9ef8dfa2824490a1c39d8fdb41899f7
hwb <37763496@qq.com>
2021-01-11 13:32:36 +0800

    1.暴露cra的webpack和craco

--- 06_learn_antdesign/craco.config.js
+++ 06_learn_antdesign/craco.config.js
@@ -1,7 +1,7 @@
 const CracoLessPlugin = require('craco-less');
 const path = require("path");
 const resolve = dir => path.resolve(__dirname, dir);
-
+/*eject这个命令可以将webpack的配置暴露出来*/
 module.exports = {
   plugins: [
     {
@@ -23,3 +23,10 @@ module.exports = {
     }
   }
 }
+/*
+* 介于cra创建的ract项目
+* 1.如果跨域可以在package.json里面配置proxy
+* 2.如果需要配置比较复杂的跨域，可以安装这个http-proxy-middleware  src/setupProxy.js去配置
+*
+*
+* */
